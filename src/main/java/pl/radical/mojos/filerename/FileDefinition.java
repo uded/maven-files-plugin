e35088375a21320a5c74b2de28a1db6a0b477b71
uded <uded@01953d5f-b815-0410-ab08-b360c6b68d4c>
2010-05-24 07:07:14 +0000

    Copy files to new project
    git-svn-id: http://svn.radical.com.pl/maven2/maven-fileshelper-plugin/trunk@2607 01953d5f-b815-0410-ab08-b360c6b68d4c

---
+++ src/main/java/pl/radical/mojos/filerename/FileDefinition.java
@@ -0,0 +1,46 @@
+package pl.radical.mojos.filerename;
+
+import java.io.File;
+
+public class FileDefinition {
+	private File inputFile;
+
+	private File outputFile;
+
+	private boolean silent;
+	
+	private boolean forceOverwrite;
+
+	public File getInputFile() {
+		return inputFile;
+	}
+
+	public void setInputFile(File inputFile) {
+		this.inputFile = inputFile;
+	}
+
+	public File getOutputFile() {
+		return outputFile;
+	}
+
+	public void setOutputFile(File outputFile) {
+		this.outputFile = outputFile;
+	}
+
+	public boolean isSilent() {
+		return silent;
+	}
+
+	public void setSilent(boolean silent) {
+		this.silent = silent;
+	}
+
+	public boolean isForceOverwrite() {
+		return forceOverwrite;
+	}
+
+	public void setForceOverwrite(boolean forceOverwrite) {
+		this.forceOverwrite = forceOverwrite;
+	}
+
+}
