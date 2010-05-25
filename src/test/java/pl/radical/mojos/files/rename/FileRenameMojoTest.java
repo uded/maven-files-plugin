fa36764bc3bbb784b1d4bf179e5cdcc072b03323
uded <uded@01953d5f-b815-0410-ab08-b360c6b68d4c>
2010-05-25 07:58:13 +0000

    Testing seems to be decent idea
    git-svn-id: http://svn.radical.com.pl/maven2/maven-files-plugin/trunk@2622 01953d5f-b815-0410-ab08-b360c6b68d4c

---
+++ src/test/java/pl/radical/mojos/files/rename/FileRenameMojoTest.java
@@ -0,0 +1,38 @@
+package pl.radical.mojos.files.rename;
+
+import static org.junit.Assert.assertTrue;
+
+import pl.radical.mojos.tests.utils.Logger;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Test;
+
+public class FileRenameMojoTest {
+
+	@Test
+	public void testSingleFileRename() throws MojoExecutionException, MojoFailureException {
+		final File output = new File("target/config.xml.rename");
+
+		final FileDefinition fd = new FileDefinition();
+		fd.setInputFile(new File("src/test/data/config.xml.tmpl"));
+		fd.setOutputFile(output);
+
+		final Set<FileDefinition> fds = new HashSet<FileDefinition>(1);
+		fds.add(fd);
+
+		final SingleFileRenameMojo mojo = new SingleFileRenameMojo();
+		mojo.setLog(new Logger());
+		mojo.filesDefinitions = fds;
+		mojo.silent = false;
+		mojo.forceOverwrite = false;
+
+		mojo.execute();
+
+		assertTrue(output.exists());
+	}
+}
